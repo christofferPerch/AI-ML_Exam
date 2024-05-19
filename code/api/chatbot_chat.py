0e4b8adfba1227b56478fee3face3e563a2c683e
christofferPerch <cph-cn332@cphbusiness.dk>
2024-05-19 11:36:09 +0200

    test

--- code/api/chatbot_chat.py
+++ code/api/chatbot_chat.py
@@ -16,49 +16,15 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 index_name = "heartdisease"
 embeddings = OpenAIEmbeddings()
 
-# pdf_path = "heart_disease2.pdf"
-# data = PyPDFLoader(pdf_path).load()
-
-# r_text_splitter = RecursiveCharacterTextSplitter(
-#     chunk_size=300,
-#     chunk_overlap=0,
-#     separators=["\n\n", "\n", " ", ""],
-# )
-# split = r_text_splitter.split_documents(data)
-
-# vectorstore = PineconeVectorStore.from_documents(
-#         split,
-#         index_name=index_name,
-#         embedding=embeddings,
-#     )
-
-heart_disease_keywords = [
-    "cardiovascular",
-    "heart",
-    "artery",
-    "hypertension",
-    "cholesterol",
-    "myocardial",
-    "angina",
-]
-
-
-def is_question_relevant(question):
-    """Check if the question contains any of the keywords related to heart disease."""
-    question_lower = question.lower()
-    return any(keyword in question_lower for keyword in heart_disease_keywords)
-
-
 def chat(input):
-    if not is_question_relevant(input):
-        return "I don't have information on that topic. Thanks for asking, master!"
-
+    
     vectorstore = PineconeVectorStore.from_existing_index(
         index_name=index_name,
         embedding=embeddings,
     )
 
-    retriever = vectorstore.as_retriever()
+    retriever=vectorstore.as_retriever(search_type="similarity_score_threshold", search_kwargs={
+                              'score_threshold': 0.5})
 
     llm = ChatOpenAI(
         openai_api_key=openai_api_key, model_name="gpt-3.5-turbo", temperature=0.0
@@ -66,8 +32,7 @@ def chat(input):
 
     template = """Use the following pieces of context to answer the question at the end.
     If you don't know the answer, just say that you don't know, don't try to make up an answer!!!.
-    Use five sentences maximum. Keep the answer as concise as possible.
-    Always say "thanks for asking, master!" at the end of the answer.
+    Use five sentences maximum. Keep the answer as concise as possible. 
 
     {context}
 
