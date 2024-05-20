d0c30b2f957692433bfef09199f5c319346d4ea5
Oscar Tuff <oscartuff@protonmail.com>
2024-05-20 12:39:44 +0200

    refactor: Update chatbot_chat.py with database connection and settings loading

--- code/api/chatbot/chatbot_chat.py
+++ code/api/chatbot/chatbot_chat.py
@@ -8,6 +8,8 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from dotenv import load_dotenv
 from langchain_pinecone import PineconeVectorStore
 import os
+from sqlalchemy import create_engine
+import pandas as pd
 
 load_dotenv()
 pinecone_api_key = os.getenv("PINECONE_API_KEY")
@@ -16,23 +18,56 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 index_name = "heartdisease"
 embeddings = OpenAIEmbeddings()
 
+
+def load_settings():
+    server_name = "localhost"
+    database_name = "HeartDisease"
+    engine = create_engine(
+        f"mssql+pyodbc://@{server_name}/{database_name}?driver=ODBC+Driver+17+for+SQL+Server&Trusted_Connection=yes"
+    )
+
+    # Define the query to select all columns except Id and CreatedAt
+    query = """
+        SELECT CB.[BasePrompt]
+      ,CB.[Temperature]
+      ,GM.[ModelName]
+  FROM [HeartDisease].[dbo].[ChatBotSettings] CB
+  INNER JOIN [HeartDisease].[dbo].[GPTModel] GM
+  ON CB.[GPTModelId] = GM.[Id]
+  WHERE CB.[Id] = 1 
+    """
+
+    # Load data into a pandas DataFrame
+    df = pd.read_sql(query, engine)
+
+    base_prompt = df["BasePrompt"].values[0]
+    temperature = df["Temperature"].values[0]
+    model_name = df["ModelName"].values[0]
+    model_name = model_name.replace("\t", "")
+
+    return base_prompt, temperature, model_name
+
+
+base_prompt, temperature, model_name = load_settings()
+
+
 def chat(input):
-    
     vectorstore = PineconeVectorStore.from_existing_index(
         index_name=index_name,
         embedding=embeddings,
     )
 
-    retriever=vectorstore.as_retriever(search_type="similarity_score_threshold", search_kwargs={
-                              'score_threshold': 0.5})
-
-    llm = ChatOpenAI(
-        openai_api_key=openai_api_key, model_name="gpt-3.5-turbo", temperature=0.0
+    retriever = vectorstore.as_retriever(
+        search_type="similarity_score_threshold", search_kwargs={"score_threshold": 0.5}
     )
 
-    template = """Use the following pieces of context to answer the question at the end.
-    If you don't know the answer, just say that you don't know, don't try to make up an answer!!!.
-    Use five sentences maximum. Keep the answer as concise as possible. 
+    llm = ChatOpenAI(
+        openai_api_key=openai_api_key, model_name=model_name, temperature=temperature
+    )
+
+    template = (
+        base_prompt
+        + """
 
     {context}
 
@@ -40,10 +75,11 @@ def chat(input):
 
     Helpful Answer:
     """
+    )
 
     custom_rag_prompt = PromptTemplate.from_template(template)
 
-    question = "Who wrote Prevalence of Uncontrolled Risk Factors for Cardiovascular Disease: United States, 1999-2010"
+    # question = "Who wrote Prevalence of Uncontrolled Risk Factors for Cardiovascular Disease: United States, 1999-2010"
 
     def format_docs(docs):
         return "\n\n".join(doc.page_content for doc in docs)
@@ -58,5 +94,5 @@ def chat(input):
     return rag_chain.invoke(input)
 
 
-response = chat("What is a heart")
-print(response)
+# response = chat("What is a heart")
+# print(response)
